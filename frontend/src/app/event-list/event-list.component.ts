bafde0745b244bdea951840fb4d9fa72b619900f
sripujithaadavi <sripujithaadavi@gmail.com>
2022-01-17 12:32:18 -0500

    event list

---
+++ frontend/src/app/event-list/event-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { EventResp, Event } from '../types/event.interface';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.scss']
+})
+export class EventListComponent implements OnInit {
+
+  mock: Event = {
+    header: "Play the fest",
+    secheader: "Come play with us",
+    desc: "dgfgfgmbfhs gzsfz gzf gzdf gdf zgfdgzsdfdAF dsfvxdfbdfbgxdgfbzf ddgbxdf",
+    sampledesc: "this is a sample desc to test how you are going to follow us",
+    postedby: "user1",
+    postedon: "1/12/2022, 1:30:08 PM",
+    price: 100.00,
+    category: "gaming",
+    address: "RitzUnion, 2nd floor 217",
+    imageUrl: "string",
+    eventon: "1/12/2022, 1:30:08 PM",
+  }
+  EventResponse: EventResp = {
+    events: [
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+      {...this.mock},
+    ]
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
