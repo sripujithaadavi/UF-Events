d5e9cc3fca857ef0d5a20ea14c9b764393a90d80
tejdeepbandreddy <tejdeepn.bandreddy@gmail.com>
2022-02-01 00:58:50 -0500

    event model added

--- BACKEND/home.go
+++ BACKEND/home.go
@@ -1,1 +1,16 @@
 package home
+
+type Events struct {
+	Header     string `json:"header"`
+	SecHeader  string `json:"secheader"`
+	Desc       string `json:"desc"`
+	SampleDesc string `json:"sampledesc"`
+	PostedBy   string `json:"postedby"`
+	PostedOn   string `json:"postedon"`
+	Price      int32  `json:"price"`
+	Category   string `json:"category"`
+	Address    string `json:"address"`
+	ImageUrl   string `json:"imageurl"`
+	EventOn    string `json:"eventon"`
+	Upvoted    bool   `json:"upvoted"`
+}
