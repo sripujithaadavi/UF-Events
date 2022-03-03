085ec70436275cc86d08b61d28460f24b50a37a6
Sri Pujitha Adavi <sripujithaadavi@gmail.com>
2022-03-02 19:00:38 -0500

    Adding Cypress Test Cases

---
+++ frontend/cypress/integration/createTest.spec.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+describe('test create function', () => {
+    beforeEach(() => {
+      cy.visit(' http://localhost:4200/create')
+    })
+
+    it('Fill the form and submit', () => {
+        cy.wait(2000)
+        cy.get('#mat-input-0').type('Mass on the Grass')
+        cy.wait(2000)
+        cy.get('#mat-input-1').type('Weekly')
+        cy.wait(2000)
+        cy.get('#mat-input-2').type('Celebrate Mass on Ash Wednesday in Campus')
+        cy.wait(2000)
+        cy.get('#mat-input-3').type('Celebrate Mass on Ash Wednesday in Campus')
+        cy.wait(2000)
+        cy.get('#mat-input-4').type('0')
+        cy.wait(2000)
+        cy.get('#mat-input-5').type('In-person')
+        cy.wait(2000)
+        cy.get('#mat-input-6').type('Plaza of the Americas')
+        cy.wait(2000)
+        cy.get('#mat-input-7').type('2022-05-03T15:00')
+        cy.wait(2000)
+    })
+
+    
+
+    it('Check if text is present', () => {
+        cy.contains('Create your Event here.')
+
+        cy.get('h1').should('have.text', 'Create your Event here.')
+        cy.get('#mat-hint-0').should('have.text','Enter the event Name')
+        cy.get('#mat-hint-1').should('have.text','Provide secondary details')
+        cy.get('#mat-hint-2').should('have.text','Please provide description')
+        cy.get('#mat-hint-3').should('have.text','Provide the event ticket price')
+        cy.get('#mat-hint-4').should('have.text','Please provide address of the Event')
+        cy.get('#mat-hint-5').should('have.text','Enter the name of the building where the event is conducted')
+        
+    })
+})
