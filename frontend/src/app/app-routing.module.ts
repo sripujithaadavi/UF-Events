a3a61338a09eaec5d773363d9a5043aa6b272f12
Sri Pujitha Adavi <sripujithaadavi@gmail.com>
2022-03-30 14:24:17 -0400

    updating routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventListComponent } from './event-list/event-list.component';
 import { FormComponent } from './form/form.component';
+import { RegisterComponent } from './register/register.component';
+import { SigninComponent } from './signin/signin.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 const routes: Routes = [
-  {path: '', component: EventListComponent},
+  {path: '', component: SigninComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'create', component: FormComponent},
-  {path: 'event:/id', component: EventListComponent}
+  {path: 'event/:id', component: EventDetailComponent},
+  {path: 'events', component: EventListComponent}
 ];
 
 @NgModule({
