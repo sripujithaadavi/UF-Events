e2c09f8facfac02b8a4549ed885e7cd7f2139bd8
Sri Pujitha Adavi <sripujithaadavi@gmail.com>
2022-04-19 02:18:27 -0400

    Updating list for filters

--- frontend/src/app/event-list/event-list.component.ts
+++ frontend/src/app/event-list/event-list.component.ts
@@ -11,27 +11,25 @@ import { Router } from '@angular/router';
 export class EventListComponent implements OnInit {
 
   showFilter = false;
-  EventResponse: EventResp = {
-    events: []
-  }
+  events = [];
   constructor(private eventsService: EventsService, private router: Router) { }
 
   ngOnInit(): void {
     this.fetchEvents();
     this.eventsService.filterMenuToggled.subscribe(res => {
       this.showFilter = res;
+    });
+
+    this.eventsService.filteredEvents.subscribe((res: any) => {
+      this.events = [...res];
     })
   }
 
   fetchEvents() {
-    this.eventsService.getEvent().subscribe((res: any[]) => {
-      this.EventResponse.events = [...res] 
-    });
-  }
-
-
-  navigatetoDetail(event) {
-    this.router.navigate(['/event/'+event.ID])
+      this.eventsService.getEvent().subscribe((res: any[]) => {
+        this.eventsService.events.next([...res]);
+        this.eventsService.filteredEvents.next([...res]);
+      });
   }
 
 }
